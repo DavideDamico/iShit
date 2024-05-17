6fa127288661b7bd492b969abe07bbce4977e2fb
DavideDamico <davidedamico89@gmail.com>
2024-05-17 19:25:18 +0200

    feat(models): creation feedback model file

---
+++ src/models/Feedback.js
@@ -0,0 +1,8 @@
+class ModelFeedback {
+    constructor(username, message, rating) {
+      this.id = this.users.length + 1;
+      this.username = username;
+      this.message = message;
+      this.rating = rating;
+    }
+  }
