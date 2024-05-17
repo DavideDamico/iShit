ce41c1d29b75ddf4bce8d1ac6f8cebf0e1a93afc
yocopk <yocopk@gmail.com>
2024-05-17 19:23:36 +0200

    feat(models): added shitcounter.js

---
+++ src/models/ShitCounter.js
@@ -0,0 +1,11 @@
+/** @format */
+
+class ModelShitCounter {
+  constructor(username, counter) {
+    this.id = id;
+    this.username = username;
+    this.counter = counter;
+    this.time = new Date().toLocaleTimeString();
+    this.date = new Date().toLocaleDateString();
+  }
+}
